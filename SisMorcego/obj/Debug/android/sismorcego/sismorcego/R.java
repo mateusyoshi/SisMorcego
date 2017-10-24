26b6249cf83fc41c0cb1bc33e3fc01c536c202e2
Unknown <mateusyoshi@gmail.com>
2017-10-24 15:31:02 -0300

    Scroll Layout
    Teste 1 do scroll layout

--- SisMorcego/obj/Debug/android/sismorcego/sismorcego/R.java
+++ SisMorcego/obj/Debug/android/sismorcego/sismorcego/R.java
@@ -36,17 +36,18 @@ public final class R {
         public static final int button2=0x7f050001;
         public static final int button3=0x7f050002;
         public static final int button4=0x7f050000;
-        public static final int button6=0x7f050006;
-        public static final int button7=0x7f050009;
-        public static final int editText1=0x7f050004;
-        public static final int editText2=0x7f050007;
+        public static final int button6=0x7f050007;
+        public static final int button7=0x7f05000a;
+        public static final int editText1=0x7f050005;
+        public static final int editText2=0x7f050008;
         public static final int imageView1=0x7f050003;
-        public static final int radioButton1=0x7f05000c;
-        public static final int radioButton2=0x7f05000d;
-        public static final int radioGroup1=0x7f05000b;
-        public static final int textView0=0x7f050005;
-        public static final int textView1=0x7f050008;
-        public static final int textView2=0x7f05000a;
+        public static final int radioButton1=0x7f05000d;
+        public static final int radioButton2=0x7f05000e;
+        public static final int radioGroup1=0x7f05000c;
+        public static final int scrollView1=0x7f050004;
+        public static final int textView0=0x7f050006;
+        public static final int textView1=0x7f050009;
+        public static final int textView2=0x7f05000b;
     }
     public static final class layout {
         public static final int equipe=0x7f030000;
